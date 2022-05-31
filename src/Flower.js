68d18d5a5e679acb373e707211baeb839ef71012
Axel Browne <dbrowne5@lion.lmu.edu>
2022-05-31 15:46:37 -0700

    green gradient flowers with symmetry, repetitions, fun shapes!

--- src/Flower.js
+++ src/Flower.js
@@ -1,13 +1,17 @@
 class Flower {
-  constructor(radialSegments = 5) {
+  constructor(radialSegments) {
     $('#flower').attr("width", $(window).width());
     $('#flower').attr("height", $(window).height());
+    this.a = 0.3;
+    this.b = 1;
+    this.a2 = 0.4;
+    this.b2 = 0.4;
+    this.radialSegments = 5;
+    this.repetitions = 3;
     this.c = document.getElementById("flower");
     this.ctx = this.c.getContext("2d");
-    this.ctx.strokeStyle = '#00FF00'
     this.originX = this.c.width / 2;
     this.originY = this.c.height / 2;
-    this.radialSegments = radialSegments;
     this.radius = Math.min(
       Math.abs(this.originX - this.c.width),
       Math.abs(this.originY - this.c.height)
@@ -15,24 +19,62 @@ class Flower {
     this.draw();
   }
 
+  drawPetal(theta) {
+    this.ctx.strokeStyle = '#00FF00';
+    // first stroke
+    this.ctx.moveTo(this.originX, this.originY);
+    this.ctx.bezierCurveTo(
+      this.bezierX(theta, this.a, this.radius * this.a2),
+      this.bezierY(theta, this.a, this.radius * this.a2),
+      this.bezierX(theta, this.b, this.radius * this.b2),
+      this.bezierY(theta, this.b, this.radius * this.b2),
+      this.bezierX(theta),
+      this.bezierY(theta)
+    );
+    // mirror stroke
+    this.ctx.moveTo(this.originX, this.originY);
+    this.ctx.bezierCurveTo(
+      this.bezierX(theta, -this.a, this.radius * this.a2),
+      this.bezierY(theta, -this.a, this.radius * this.a2),
+      this.bezierX(theta, -this.b, this.radius * this.b2),
+      this.bezierY(theta, -this.b, this.radius * this.b2),
+      this.bezierX(theta),
+      this.bezierY(theta)
+    );
+    this.ctx.stroke();
+    var grd = this.ctx.createRadialGradient(this.originX, this.originY, this.radius/5, this.originX, this.originY, this.radius)
+    // gradient color should be also a function of which repetition we are on
+    // that way we can make the "leaves" green
+    // but we can make the flowers colorful
+    grd.addColorStop(0, '#005500');
+    grd.addColorStop(1, '#00aa00');
+    this.ctx.fillStyle = grd;
+    this.ctx.fill();
+  }
+
+  bezierX(theta, offset = 0, radius = this.radius) {
+    return this.originX + (radius * Math.cos(theta + offset)); 
+  }
+
+  bezierY(theta, offset = 0, radius = this.radius) {
+    return this.originY + (radius * Math.sin(theta + offset))
+  }
+
   draw() {
-    let theta, thetaInc, i, endX, endY;
-    thetaInc = (2 * Math.PI) / this.radialSegments
-    // this.ctx.moveTo(this.originX, this.originY);
-    // this.ctx.bezierCurveTo(this.originX, this.originY, this.originX, this.originY, this.radius * Math.cos(0), this.radius * Math.sin(0));
-    // this.ctx.stroke();
-    for (i = 0; i < this.radialSegments; i++) {
-      this.ctx.moveTo(this.originX, this.originY)
-      theta = i * thetaInc;
-      endX = this.originX + (this.radius * Math.cos(theta));
-      endY = this.originY + (this.radius * Math.sin(theta));
-      this.ctx.bezierCurveTo(endX, endY, endX, endY, endX, endY);
-      this.ctx.stroke();
-      // this.ctx.noStroke();
+    let theta, i, segmentOffset, radiusDec, j, repOffset;
+    segmentOffset = ((2 * Math.PI) / this.radialSegments);
+    // symmetry is best when the divisor is 2
+    repOffset = ((2 * Math.PI) / 2);
+    // might want to adjust how quickly radius decreases
+    // controlled by gene?
+    radiusDec = this.radius / (this.repetitions);
+    for (j = 0; j < this.repetitions; j++) {
+      for (i = 0; i < this.radialSegments; i++) {
+        theta = (i * segmentOffset) + (j * repOffset);
+        this.drawPetal(theta)
+      }
+      this.radius -= radiusDec;
     }
-    
-
-
   }
 
   getOriginX() {
